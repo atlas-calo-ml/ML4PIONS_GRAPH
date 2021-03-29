24d0a8baedceb097e0c369c0d6689eb0ff1dd802
sanmayphy <sanmay.ganguly@gmail.com>
2021-03-29 16:22:52 +0300

    adding basic evaluation script

---
+++ evaluate_ml4pions.py
@@ -0,0 +1,97 @@
+import dgl
+import dgl.function as fn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, Sampler
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import math
+
+import uproot#3 as uproot
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+import h5py
+
+torch.manual_seed(0)
+
+import os, sys
+
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
+cuda_device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu' )
+
+print('cuda_device : ', cuda_device)
+
+from modules.ML4Pions_Dataset import  MLPionsDataset_KNN, collate_graphs
+
+from modules.dynamic_graph import Dynamic_Graph_Model
+
+cluster_var = ['cluster_EM_PROBABILITY', 'cluster_HAD_WEIGHT', 'cluster_OOC_WEIGHT',
+               'cluster_DM_WEIGHT', 'cluster_CENTER_MAG', 'cluster_FIRST_ENG_DENS', 
+               'cluster_CENTER_LAMBDA', 'cluster_ISOLATION',  
+              ]
+
+file_name_test = 'samples/ml4pions_test.root'
+
+test_data = MLPionsDataset_KNN(filename=file_name_test, k_val=5, cluster_var=cluster_var, num_ev=1000)
+
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=False,collate_fn=collate_graphs, num_workers=0)
+
+model = Dynamic_Graph_Model(feature_dims_x = [8, 9, 7, 5], feature_dims_en = [4, 5, 6, 8])
+#model = nn.DataParallel(model)
+model.to(cuda_device)
+
+model.load_state_dict(torch.load('model_DynamicGraph.pt'))
+model.eval()
+
+pred_energy, target_energy = [], []
+tot_pred_energy, tot_target_energy = [], []
+
+with tqdm(test_loader, ascii=True) as tq:
+    for gr, truth_calib in tq:
+        
+        gr, truth_calib = gr.to(cuda_device), truth_calib.to(cuda_device)
+        
+        pred_calib = model( gr )
+
+        pred_energy.append( pred_calib.cpu().detach().numpy() )
+        target_energy.append( truth_calib.cpu().detach().numpy() )
+        
+        tot_pred_energy.append( np.sum(pred_calib.cpu().detach().numpy()) )
+        tot_target_energy.append( np.sum(truth_calib.cpu().detach().numpy()) )
+        
+        
+        del gr; del truth_calib; del pred_calib;
+
+
+pred_energy, target_energy = np.concatenate( pred_energy ), np.concatenate( target_energy )
+
+pred_energy = pred_energy[ np.where(target_energy!=0) ]
+target_energy = target_energy[ np.where(target_energy!=0) ]
+
+resp = (pred_energy)/target_energy
+
+print('mean R : ', resp.mean() )
+print('std R : ', resp.std() )
+
+tot_pred_energy, tot_target_energy = np.array(tot_pred_energy), np.array(tot_target_energy)
+tot_pred_energy = tot_pred_energy[np.where(tot_target_energy!=0)]
+tot_target_energy = tot_target_energy[np.where(tot_target_energy!=0)]
+
+resp_tot = tot_pred_energy/tot_target_energy
+
+print('total mean R : ', resp_tot.mean() )
+print('total std R : ', resp_tot.std() )
+
+
+hf = h5py.File('PredictionFile_DynamicGraph.h5', 'w')
+
+hf.create_dataset('pred_energy', data=pred_energy, compression='lzf')
+hf.create_dataset('target_energy', data=target_energy, compression='lzf')
+hf.create_dataset('tot_pred_energy', data=tot_pred_energy, compression='lzf')
+hf.create_dataset('tot_target_energy', data=tot_target_energy, compression='lzf')
+
+hf.close()
