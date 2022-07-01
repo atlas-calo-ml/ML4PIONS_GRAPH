9309bf8896cf5b5b7d74f30d3925310361122c7b
Sanmay Ganguly <sanmay.ganguly@gmail.com>
2022-07-01 18:24:06 +0900

    Update evaluate_ml4pions.py

--- evaluate_ml4pions.py
+++ evaluate_ml4pions.py
@@ -32,7 +32,7 @@ cuda_device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu' )
 
 print('cuda_device : ', cuda_device)
 
-from modules.ML4Pions_Dataset import  MLPionsDataset_KNN, collate_graphs
+from modules.ML4Pions_Dataset import  MLPionsDataset_KNN, collate_graphs, collate_graphs_eta
 
 from modules.dynamic_graph import Dynamic_Graph_Model
 from modules.attention_graph import Graph_Attention_Model
@@ -42,21 +42,53 @@ cluster_var = ['cluster_EM_PROBABILITY', 'cluster_HAD_WEIGHT', 'cluster_OOC_WEIG
                'cluster_CENTER_LAMBDA', 'cluster_ISOLATION',  
               ]
 
-file_name_test = 'samples/ml4pionsJet_test.root'
+track_var = ['trackPt',
+             'trackP',
+             'trackMass',
+             'trackEta',
+             'trackPhi',
+             'trackNumberOfPixelHits',
+             'trackNumberOfSCTHits',
+             'trackNumberOfPixelDeadSensors',
+             'trackNumberOfSCTDeadSensors',
+#              'trackNumberOfPixelSharedHits',
+#              'trackNumberOfSCTSharedHits',
+#              'trackNumberOfPixelHoles',
+#              'trackNumberOfSCTHoles',
+             'trackNumberOfInnermostPixelLayerHits',
+             'trackNumberOfNextToInnermostPixelLayerHits',
+             'trackExpectInnermostPixelLayerHit',
+             'trackExpectNextToInnermostPixelLayerHit',
+             'trackNumberOfTRTHits',
+             'trackNumberOfTRTOutliers',
+             'trackChiSquared',
+             'trackNumberDOF',
+             'trackD0',
+             'trackZ0'
+            ]
 
-test_data = MLPionsDataset_KNN(filename=file_name_test, k_val=5, cluster_var=cluster_var, num_ev=500)
+file_name_test = 'samples/ml4pions_test.root'
 
-test_loader = torch.utils.data.DataLoader(test_data, batch_size=1, shuffle=False,collate_fn=collate_graphs, num_workers=0)
+n_test = 50000
+n_slice = 1000
+
+#test_data = MLPionsDataset_KNN(filename=file_name_test, k_val=5, cluster_var=cluster_var+track_var, num_ev=200000)
+test_data = torch.utils.data.ConcatDataset([
+            MLPionsDataset_KNN(filename=file_name_test, k_val=5, cluster_var=cluster_var+track_var, nstart=i, nstop=i+n_slice)\
+            for i in range( 0, n_test, n_slice )            
+    ])
+
+test_loader = DataLoader(test_data, batch_size=100, shuffle=False,collate_fn=collate_graphs_eta, num_workers=0)
 
 #model = Dynamic_Graph_Model(feature_dims_x = [8, 9, 7, 5], feature_dims_en = [4, 5, 6, 8])
 if(model_name == 'edgeconv') : 
-    model = Dynamic_Graph_Model(feature_dims_x = [8, 9, 7, 5], feature_dims_en = [4, 5, 6, 8])
-    model_name = 'model_DynamicGraphJet.pt'
-    out_name = 'PredictionFile_DynamicGraphJet.h5'
+    model = Dynamic_Graph_Model(feature_dims_x = [8, 9, 10, 7, 5], feature_dims_en = [4, 5, 7, 6, 8], input_names=cluster_var+track_var)
+    model_name = 'model_DynamicGraphTP.pt'
+    out_name = 'PredictionFile_DynamicGraphJetTP.h5'
 else : 
-    model = Graph_Attention_Model(num_heads = 5, feature_dims = [10, 15, 12, 8], input_names=cluster_var)
-    model_name = 'model_AttentionGraphJet.pt'
-    out_name = 'PredictionFile_AttentionGraphJet.h5'
+    model = Graph_Attention_Model(num_heads = 5, feature_dims = [10, 15, 12, 10, 8], input_names=cluster_var+track_var)
+    model_name = 'model_AttentionGraphTP.pt'
+    out_name = 'PredictionFile_AttentionGraphTP_New.h5'
 #model = nn.DataParallel(model)
 model.to(cuda_device)
 
@@ -67,11 +99,11 @@ print('Total parameters : ', param_numb)
 
 model.eval()
 
-pred_energy, target_energy = [], []
+pred_energy, target_energy, truth_eta = [], [], []
 tot_pred_energy, tot_target_energy = [], []
 
 with tqdm(test_loader, ascii=True) as tq:
-    for gr, truth_calib in tq:
+    for gr, truth_calib, eta in tq:
         
         gr, truth_calib = gr.to(cuda_device), truth_calib.to(cuda_device)
         
@@ -79,22 +111,25 @@ with tqdm(test_loader, ascii=True) as tq:
 
         pred_energy.append( pred_calib.cpu().detach().numpy() )
         target_energy.append( truth_calib.cpu().detach().numpy() )
+        truth_eta.append( eta.cpu().detach().numpy() )
         
         tot_pred_energy.append( np.sum(pred_calib.cpu().detach().numpy()) )
         tot_target_energy.append( np.sum(truth_calib.cpu().detach().numpy()) )
         
         
-        del gr; del truth_calib; del pred_calib;
+        del gr; del truth_calib; del pred_calib; del eta;
 
 
 pred_energy, target_energy = np.concatenate( pred_energy ), np.concatenate( target_energy )
+truth_eta = np.concatenate(truth_eta)
 
 pred_energy = pred_energy[ np.where(target_energy!=0) ]
 target_energy = target_energy[ np.where(target_energy!=0) ]
+truth_eta = truth_eta[ np.where(target_energy!=0) ]
 
 resp = (pred_energy)/target_energy
 
-print('mean R : ', resp.mean() )
+print('mean R : ', np.median( resp ) )
 print('std R : ', resp.std() )
 
 tot_pred_energy, tot_target_energy = np.array(tot_pred_energy), np.array(tot_target_energy)
@@ -103,7 +138,7 @@ tot_target_energy = tot_target_energy[np.where(tot_target_energy!=0)]
 
 resp_tot = tot_pred_energy/tot_target_energy
 
-print('total mean R : ', resp_tot.mean() )
+print('total mean R : ', np.median( resp_tot ) )
 print('total std R : ', resp_tot.std() )
 
 
@@ -111,6 +146,7 @@ hf = h5py.File(out_name, 'w')
 
 hf.create_dataset('pred_energy', data=pred_energy, compression='lzf')
 hf.create_dataset('target_energy', data=target_energy, compression='lzf')
+hf.create_dataset('truth_eta', data=truth_eta, compression='lzf')
 hf.create_dataset('tot_pred_energy', data=tot_pred_energy, compression='lzf')
 hf.create_dataset('tot_target_energy', data=tot_target_energy, compression='lzf')
 
